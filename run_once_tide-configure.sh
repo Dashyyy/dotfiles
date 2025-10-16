fb2dd8f6f74fb9e9f1aeae3489d9201f1c1cda35
Dashyyy <j.kobylanski@proton.me>
2025-10-16 18:00:23 +0200

    add tide configuration

---
+++ run_once_tide-configure.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+tide configure --auto \
+    --style=Rainbow \
+    --prompt_colors='True color' \
+    --show_time=No \
+    --rainbow_prompt_separators=Round \
+    --powerline_prompt_heads=Sharp \
+    --powerline_prompt_tails=Flat \
+    --powerline_prompt_style='Two lines, character and frame' \
+    --prompt_connection=Solid \
+    --powerline_right_prompt_frame=Yes \
+    --prompt_connection_andor_frame_color=Darkest \
+    --prompt_spacing=Sparse \
+    --icons='Many icons' \
+    --transient=No
